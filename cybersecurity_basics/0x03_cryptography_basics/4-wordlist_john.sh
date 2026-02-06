72baff72d2caf7132a5212cb4893fc74f5a7417c
abbasovv0 <abbasabbasov569@gmail.com>
2026-02-06 08:31:09 -0500

    Wordlist Mode

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: 'NR>1 && NF{print $2}' > 4-password.txt
