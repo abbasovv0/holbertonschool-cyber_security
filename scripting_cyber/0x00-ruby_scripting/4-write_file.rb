18f3d1eb7874a8be8f9c37efa4adcadfec9fc8d0
abbasovv0 <abbasabbasov569@gmail.com>
2026-05-20 17:10:53 -0400

    Writing to a File

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,8 +1,8 @@
 require 'json'
 
-def merge_json_files(3-read_file, 4-write_file)
-  file1 = File.read(3-read_file)
-  file2 = File.read(3-write_file)
+def merge_json_files(file1_path, file2_path)
+  file1 = File.read(file1_path)
+  file2 = File.read(file2_path)
 
   data1 = JSON.parse(file1)
   data2 = JSON.parse(file2)
@@ -10,4 +10,5 @@ def merge_json_files(3-read_file, 4-write_file)
   merged = data2 + data1
 
   File.write(file2_path, JSON.generate(merged))
+  puts "Merged JSON written to #{file2_path}"
 end
